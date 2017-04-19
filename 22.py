2d8bd6f70e1603d8a1e0de69fcecf1da59357dd2
tacchan7412 <tacchan.04@gmail.com>
2017-04-20 08:51:32 +0900

    add 22-23

---
+++ 22.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+from modules import extract_British_from_json
+import re
+
+if __name__ == '__main__':
+    lines = extract_British_from_json().split("\n")
+    for line in lines:
+        if "Category" in line:
+            print(re.search('^\[\[Category:(.*?)(\|.*)*\]\]', line).group(1))
---
+++ 23.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+from modules import extract_British_from_json
+import re
+
+if __name__ == '__main__':
+    lines = extract_British_from_json().split("\n")
+    for line in lines:
+        section_line = re.search('^(=+)(.*?)(=+)', line)
+        if section_line is not None:
+            if section_line.group(1) != section_line.group(3):
+                break
+            print(section_line.group(2).strip(), len(section_line.group(1)) - 1)
