c06c43b5ef70b4c605bab72de1d9949d3968c8d2
tacchan7412 <tacchan.04@gmail.com>
2017-04-21 17:51:59 +0900

    add 25

---
+++ 25.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+from modules import extract_British_from_json
+import re
+
+if __name__ == '__main__':
+    map = {}
+    lines = re.split('\n\||\n\}', extract_British_from_json())
+    for line in lines:
+        temp_line = re.search('(.*) = (.*)', line, re.S)
+        if temp_line is not None:
+            map[temp_line.group(1)] = temp_line.group(2)
+    for key, value in map.items():
+        print(key, value)
