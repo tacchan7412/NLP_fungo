67c4e5ba876c3728951b20e5d9e0526c24957b5f
tacchan7412 <tacchan.04@gmail.com>
2017-04-26 19:04:27 +0900

    add 30 and update modules

--- modules.py
+++ modules.py
@@ -10,3 +10,25 @@ def extract_British_from_json():
         line = f.readline()
     f.close()
     return ""
+
+def read_mecab():
+    f = open('neko.txt.mecab', 'r')
+    lines = f.readlines()
+    morphologies = []
+    sentence = []
+    for line in lines:
+        if 'EOS' in line:
+            morphologies.append(sentence)
+            sentence = []
+        else:
+            line_split = line.split('\t')
+            surface = line_split[0]
+            elems = line_split[1].split(',')
+            sentence.append({
+                'surface': surface,
+                'base': elems[6],
+                'pos': elems[0],
+                'pos1': elems[1]
+            })
+    f.close()
+    return morphologies
