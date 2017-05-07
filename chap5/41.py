52382546212ff511ba5f8a87ecdb7be6c47d6b76
tacchan7412 <tacchan.04@gmail.com>
2017-05-07 14:03:57 +0900

    add 41 & update 40

--- chap5/41.py
+++ chap5/41.py
@@ -12,27 +12,43 @@ class Morph():
     def __str__(self):
         return 'surface: {}, base: {}, pos: {}, pos1: {}'.format(self.surface, self.base, self.pos, self.pos1)
 
-def read_cabocha_morph():
+class Chunk():
+    def __init__(self, morphs, dst, srcs):
+        self.morphs = morphs
+        self.dst = int(dst)
+        self.srcs = int(srcs)
+
+    def __str__(self):
+        return 'dst: {}, srcs: {}, morphs: {}'.format(self.dst, self.srcs, ' | '.join([str(morph) for morph in self.morphs]))
+
+def read_cabocha_chunk():
     f = open('data/neko.txt.cabocha', 'r')
     lines = f.readlines()
     sentences = []
+    chunk = None
     sentence = []
     for line in lines:
         if 'EOS' in line:
+            sentence.append(chunk)
+            chunk = None
             sentences.append(sentence)
             sentence = []
         elif '* ' in line:
-            continue
+            if chunk is not None:
+                sentence.append(chunk)
+                chunk = None
+            line_split = line.split()
+            chunk = Chunk([], line_split[2].rstrip('D'), line_split[1])
         else:
             line_split = line.split('\t')
             surface = line_split[0]
             elems = line_split[1].split(',')
             morph = Morph(surface, elems[6], elems[0], elems[1])
-            sentence.append(morph)
+            chunk.morphs.append(morph)
     f.close()
     return sentences
 
 if __name__ == '__main__':
-    sentences = read_cabocha_morph()
-    for morph in sentences[2]:
-        print(str(morph))
+    sentences = read_cabocha_chunk()
+    for chunk in sentences[2]:
+        print(str(chunk))
