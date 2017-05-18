1262891db5526d490ececef57386a74193482702
tacchan7412 <tacchan.04@gmail.com>
2017-05-18 11:09:03 +0900

    update 53 & add 54:

--- chap6/53.py
+++ chap6/53.py
@@ -2,11 +2,11 @@
 import os, sys
 sys.path.append(os.getcwd())
 
-import re
+import xml.etree.ElementTree as ET
+
 if __name__ == '__main__':
-    f = open('data/nlp.txt.xml', 'r')
-    lines = f.readlines()
-    for line in lines:
-        if '<word>' in line and '</word>' in line:
-            re_line = re.search('<word>(.+?)</word>', line)
-            print(re_line.group(1))
+    tree = ET.parse('data/nlp.txt.xml')
+    root = tree.getroot()
+    elem_list = root.findall(".//word")
+    for elem in elem_list:
+        print(elem.text)
