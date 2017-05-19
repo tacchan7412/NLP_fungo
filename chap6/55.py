e705a773498a7235f8e457bca2235c4980471366
tacchan7412 <tacchan.04@gmail.com>
2017-05-19 09:56:48 +0900

    add 55

--- chap6/55.py
+++ chap6/55.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     tree = ET.parse('data/nlp.txt.xml')
     root = tree.getroot()
     word_list = root.findall(".//word")
-    lemma_list = root.findall(".//lemma")
-    pos_list = root.findall(".//POS")
-    for word, lemma, pos in zip(word_list, lemma_list, pos_list):
-        print('%s\t%s\t%s'%(word.text, lemma.text, pos.text))
+    ner_list = root.findall(".//NER")
+    for word, ner in zip(word_list, ner_list):
+        if ner.text == 'PERSON':
+            print(word.text)
