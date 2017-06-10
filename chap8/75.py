231aff0a424b0764d0036a317483db286a2c3b3d
tacchan7412 <tacchan.04@gmail.com>
2017-06-10 16:40:32 +0900

    add 75

---
+++ chap8/75.py
@@ -0,0 +1,63 @@
+# coding: utf-8
+import os, sys
+sys.path.append(os.getcwd())
+
+import codecs
+import collections
+import math
+from nltk import stem
+
+
+eta0 = 0.66
+etan = 0.9999
+guard = 0.0002
+
+f1 = codecs.open('data/stop_list.txt', 'r', 'latin_1')
+stop_list = [x[:-1] for x in f1.readlines()]
+f1.close()
+
+def isinStopList(word):
+    return word in stop_list
+
+def sigmoid(x):
+  return 1.0 / (1.0 + math.exp(-x))
+
+def update(W, features, label, eta):
+  a = sum([W[x] for x in features])
+  init_feature = 1
+  predict = sigmoid(a)
+  label = (label + 1) / 2
+
+  for x in features:
+    dif = eta * ( predict -label ) * init_feature
+    if (W[x] - dif) > guard or (W[x] - dif) < (guard * -1):
+      W[x] = W[x] - dif
+
+if __name__ == "__main__":
+
+    stemmer = stem.PorterStemmer()
+    f2 = codecs.open('data/sentiment.txt', 'r', 'latin_1')
+    sentiments = f2.readlines()
+
+    features_list = []
+    for sentiment in sentiments:
+        features = [sentiment.split()[0]]
+        for word in sentiment.split()[1:]:
+            word = stemmer.stem(word)
+            if not isinStopList(word):
+                features.append(word)
+        features_list.append(features)
+
+    W = collections.defaultdict(float)
+    for t, features in enumerate(features_list):
+        update(W, features[1:], float(features[0]), eta0 * (etan**t))
+
+    sorted_W = sorted(W.items(), key=lambda x:x[1])
+    
+    print('重みの高い素性')
+    for k, v in sorted_W[::-1][:10]:
+        print(k, v)
+
+    print('重みの低い素性')
+    for k, v in sorted_W[:10]:
+        print(k, v)
