3171ee91b28426db9b400458487c5685762d7ab3
tacchan7412 <tacchan.04@gmail.com>
2017-06-13 13:37:05 +0900

    update 77

--- chap8/77.py
+++ chap8/77.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     f = codecs.open('data/76.out', 'r')
     lines = f.readlines()
 
-    TP, FP, FN = 0, 0, 0
+    TP, FP, FN, TN = 0, 0, 0, 0
     for line in lines:
         [test_y, pred_y, _] = line.split('\t')
         if pred_y == '+1' and test_y == '+1':
@@ -20,9 +20,12 @@ if __name__ == "__main__":
             FN += 1
         elif pred_y == '+1' and test_y == '-1':
             FP += 1
+        else:
+            TN += 1
 
+    score = (TP + TN) / len(lines)
     precision = TP / (TP + FN)
     recall = TP / (TP + FP)
     f1_score = 2 / (1 / precision + 1 / recall)
 
-    print("precision:",precision,"\nrecall:",recall,"\nf1 score:",f1_score)
+    print("score",score, "\nprecision:",precision,"\nrecall:",recall,"\nf1 score:",f1_score)
